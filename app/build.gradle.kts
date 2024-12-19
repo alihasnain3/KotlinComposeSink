08668445407949f52339455e41990300543a6b37
alihasnain <alihasnain3@gmail.com>
2024-12-19 17:17:45 +0500

    Setting compileSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.app.kotlincomposesink"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.kotlincomposesink"
